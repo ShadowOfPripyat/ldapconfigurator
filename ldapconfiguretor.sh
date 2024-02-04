111683159f3a7c41ccaa77b47595873d0fb90ebe
Vasily Kalashinov <73404930+ShadowOfPripyat@users.noreply.github.com>
2024-02-05 00:58:15 +0100

    graphical lmao

--- ldapconfiguretor.sh
+++ ldapconfiguretor.sh
@@ -2,6 +2,65 @@
 
 # while-menu-dialog: a menu driven system information program
 
+##########FUNCTIONS##########
+install_ldap_packages() {
+    echo "Installing LDAP packages..."
+    sudo apt-get update
+    sudo apt-get install -y ldap-utils slapd ldapscripts ldap-account-manager
+    echo "LDAP packages installed successfully!"
+}
+#´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´
+make_ldif_examples() {
+    echo "Creating ldif examples..."
+    cd ~
+
+# OU LDIF EXAMPLE
+    cat << EOF > ou.ldif
+dn: ou=UnitatOrganitzativa,dc=domini,dc=cat
+objectClass: organizationalUnit
+objectClass: top
+ou: UnitatOrganitzativa
+EOF
+
+# GROUP LDIF EXAMPLE
+    cat << EOF > grup.ldif
+# Es pot fer servir un dels 2 DN, un per posar-lo dins a una Unitat Organitzativa i l'altre no.
+# dn: cn=Grup,ou=UnitatOrganitzativa,dc=domini,dc=cat
+dn: cn=Grup,dc=domini,dc=cat
+objectClass: top
+objectClass: posixGroup
+gidNumber: 2000
+cn: Grup
+EOF
+
+# USER LDIF EXAMPLE
+    cat << EOF > usuari.ldif
+# Es pot fer servir un dels 2 DN, un per posar-lo dins a una Unitat Organitzativa i l'altre no.
+# dn: uid=Usuari,ou=UnitatOrganitzativa,dc=domini,dc=cat
+dn: uid=Usuari,dc=domini,dc=cat
+objectClass: top
+objectClass: posixAccount
+objectClass: inetOrgPerson
+objectClass: person
+cn: Usuari
+uid: Usuari
+uidNumber 2000
+#gidNumber: 2000
+#homeDirectory: /home/usuari
+#loginShell: /bin/bash
+#sn: Cognom
+# passwrod should be generated with slappasswd or similar...
+userPassword: caput
+
+EOF
+echo "Added ou.ldif at ~"
+echo "Added grup.ldif at ~"
+echo "Added usuari.ldif at ~"
+}
+#´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´
+
+#--------------------------------------------------START--GRAPHICAL--INTERFACE------------------------------------------------------#
+
 DIALOG_CANCEL=1
 DIALOG_ESC=255
 HEIGHT=0
@@ -17,12 +76,12 @@ while true; do
   exec 3>&1
   selection=$(dialog \
     --backtitle "System Information" \
-    --title "Menu" \
+    --title "LDAP CONFIGURATOR V1-G" \
     --clear \
     --cancel-label "Exit" \
     --menu "Please select:" $HEIGHT $WIDTH 4 \
-    "1" "Display System Information" \
-    "2" "Display Disk Space" \
+    "1" "Instalar tots els paquets ldap" \
+    "2" "Crear Fitxers ldif" \
     "3" "Display Home Space Utilization" \
     2>&1 1>&3)
   exit_status=$?
@@ -41,12 +100,12 @@ while true; do
   esac
   case $selection in
     1 )
-      result=$(echo "Hostname: $HOSTNAME"; uptime)
-      display_result "System Information"
+      result=$(install_ldap_packages)
+      display_result "Instalació de Paquets LDAP"
       ;;
     2 )
-      result=$(df -h)
-      display_result "Disk Space"
+      result=$(make_ldif_examples)
+      display_result "Make LDIF Examples"
       ;;
     3 )
       if [[ $(id -u) -eq 0 ]]; then
@@ -59,3 +118,4 @@ while true; do
       ;;
   esac
 done
+#-----------------------------------------------------END--GRAPHICAL--INTERFACE------------------------------------------------------#
